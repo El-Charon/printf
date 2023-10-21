dd232643fe310a70a63ce92b6877c3daba6cb499
El-Charon <70296200+El-Charon@users.noreply.github.com>
2023-10-20 19:21:56 -0700

    test

--- _printf.c
+++ _printf.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 int _printf(const char *format, ...) {
+    int count = 0;
     va_list args;
     va_start(args, format);
-    int count = 0;
 
     while (*format) {
         if (*format != '%') {
